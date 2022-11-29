1c04317e46e46e734e13779620a57aae1277f74c
Jyotiprakash <jpjena@gmail.com>
2022-11-29 12:51:53 +0530

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports.generateRandomNumber = event => {
+ const randomNumber = parseInt(Math.random()*100);
+ console.log('The Random Generated Number is: ', randomNumber);
+ return randomNumber;
+};
